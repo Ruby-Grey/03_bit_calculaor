8d55250c339d9adf5715d7a7819ac67e932c4ede
Ruby-Grey <141281186+Ruby-Grey@users.noreply.github.com>
2023-09-08 14:39:48 +1200

    Add files via upload

--- 03_integer_checker.py
+++ 03_integer_checker.py
@@ -1,4 +1,4 @@
-# Checks that number is more that zero
+# Checks that number is more than a given value
 def num_check(question, low):
     valid = False
     while not valid:
@@ -22,3 +22,18 @@ def num_check(question, low):
 
         except ValueError:
             print(error)
+
+# Main routine goes here
+
+keep_going = ""
+while keep_going == "":
+    print()
+    # ask user for an integer (must be more than / equal to 0)
+    var_integer = num_check("Please enter an integer: ", 0)
+    print()
+
+    # ask for width & height of an image
+    # (must be more than / equal to 1)
+    image_width = num_check("Image width? ", 1)
+    print()
+    image_height = num_check("Image height? ", 1)
