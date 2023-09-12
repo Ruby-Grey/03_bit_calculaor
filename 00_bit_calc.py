63203f0629cda918fc6de1eb69aa8e190a4a4f33
Ruby-Grey <141281186+Ruby-Grey@users.noreply.github.com>
2023-09-12 12:24:38 +1200

    Add files via upload

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -5,7 +5,6 @@ def statement_generator(text, decoration):
     # Make a string with five characters
     ends = decoration * 5
 
-
     # add decoration to start and end of statement
     statement = "{} {} {}".format(ends, text, ends)
 
@@ -80,15 +79,80 @@ def num_check(question, low):
             print(error)
 
 
+# calculates the # of bits for text (# of characters x 8)
+def text_bits():
+
+    print()
+    # ask user for a string...
+    var_text = input("Enter some text: ")
+
+    # calculate # of bits (length of string x 8)
+    var_length = len(var_text)
+    num_bits = 8 * var_length
+
+    # output answer with working
+    print()
+    print("\'{}\' has {} characters ...".format(var_text, var_length))
+    print("# of bits is {} x 8...".format(var_length))
+    print("We need {} bits to represent '{}'".format(num_bits, var_text))
+    print()
+
+    return ""
+
+
+# finds # of bits for 24 bit colour
+def image_bits():
+    # asks user for width and height
+
+    image_height = num_check("Enter the height: ", 1)
+    image_width = num_check("Enter the width: ", 1)
+
+    # calculate pixels and bits
+
+    num_pixels = image_width * image_height
+    num_bits = num_pixels * 24
+
+    print()
+    print("# of pixels = {} x {} = {}".format(image_height, image_width, num_pixels))
+    print("# bits = {} x 24 = {}".format(num_pixels, num_bits))
+    print()
+
+    return ""
+
+
+# converts decimal to binary and states how many
+# bits are needed to represent the original integer
+def int_bits():
+
+    # get integer (must be >= 0)
+    var_integer = num_check("Please enter an integer: ", 0)
+
+    # source for code
+    # https://stackoverflow.com/questions/699866/python-int-to-binary-string
+
+    var_binary = "{0:b}.".format(var_integer)
+
+    # calculate # of bits (length of string above)
+    num_bits = len(var_binary)
+
+    # output answer with working
+    print()
+    print("{} in binary is {}".format(var_integer, var_binary))
+    print("# of bits is {}".format(num_bits))
+    print()
+
+    return ""
+
+
 # Main Routine goes here
 
 # Heading
-statement_generator("Bit Calculator for Integers, Text & Images,","-")
+statement_generator("Bit Calculator for Integers, Text & Images,", "-")
 
 # Display instructions if user has not used the program before
 
 # Loop to allow multiple calculations per session
-keep_going ""
+keep_going = ""
 while keep_going == "":
 
     # Ask the user for the file type
@@ -97,19 +161,23 @@ while keep_going == "":
 
     # For integers, ask for integer
     if data_type == "integer":
-        var_integer = num_check("Enter an integer: ", 0)
+        int_bits()
 
     # For images, ask for width and height
     # (must be integers more than / equal to 1
     elif data_type == "image":
-        image_width = num_check("Enter the width: ", 1)
-        print()
-        image_height = num_check("Enter the height: ", 1)
+        image_bits()
 
     # For text, ask for a string
     else:
-        var_text = input("Enter some text: ")
+        text_bits()
+
+    print()
+    keep_going = input("Press <enter> to continue or any key to quit ")
+    print()
 
     # (must be integers more than / equal to 1)
 
-# For text, ask for a string
+    # For text, ask for a string
+
+
