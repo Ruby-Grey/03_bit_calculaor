3c43b5d598a66023b2669e4248aac9b8f5458b70
Ruby-Grey <141281186+Ruby-Grey@users.noreply.github.com>
2023-09-20 10:26:08 +1200

    Add files via upload

---
+++ 07_instructions.py
@@ -0,0 +1,34 @@
+# put series of symbols at start and end of text (for emphasis)
+def statement_generator(text, decoration):
+    # Make a string with five characters
+    ends = decoration * 5
+
+    # add decoration to start and end of statement
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+
+# displays instructions / information
+def instructions():
+
+    statement_generator("Instructions / Information", "=")
+    print()
+    print("Please choose a data type (image / text / integer)")
+    print()
+    print("This program assumes that images are being represented in 24 bit colour "
+          "(ie: 24 bits per pixel). For text, we assume that ascii encoding "
+          "is being used (8 bits per character)")
+    print()
+    print("Complete as many calculations as necessary, pressing <enter> at "
+          "the end of each calculations or any key to quit.")
+    print()
+    return ""
+
+
+# Main routine
+instructions()
