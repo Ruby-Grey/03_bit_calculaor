05b8824e05df95040529413b7342756518320c6c
Ruby-Grey <141281186+Ruby-Grey@users.noreply.github.com>
2023-09-20 10:33:47 +1200

    Add files via upload

--- 07_instructions.py
+++ 07_instructions.py
@@ -20,12 +20,12 @@ def instructions():
     print()
     print("Please choose a data type (image / text / integer)")
     print()
-    print("This program assumes that images are being represented in 24 bit colour "
-          "(ie: 24 bits per pixel). For text, we assume that ascii encoding "
-          "is being used (8 bits per character)")
+    print("This program assumes that images are being represented in 24 bit colour")
+    print("(ie: 24 bits per pixel). For text, we assume that ascii encoding")
+    print("is being used (8 bits per character)")
     print()
-    print("Complete as many calculations as necessary, pressing <enter> at "
-          "the end of each calculations or any key to quit.")
+    print("Complete as many calculations as necessary, pressing <enter> at ")
+    print("the end of each calculations or any key to quit.")
     print()
     return ""
 
